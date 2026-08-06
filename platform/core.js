bbf053c0f7a694b9e73a94cbb6b48b230da062c2
itzpeckin <justinrmandaro@gmail.com>
2026-08-06 14:56:54 -0500

    5.9.1.5 - Platform

--- platform/core.js
+++ platform/core.js
@@ -17,7 +17,7 @@
   const metadata = Object.freeze({
     name: 'Franchise HQ',
     architecture: 'Frontend Architecture v2',
-    version: '5.9.1.4',
+    version: '5.9.1.5',
     release: 5,
     epic: 9,
     patch: 1,
