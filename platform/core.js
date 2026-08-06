f9711416468d5aafad3e0bc09cd50ecf680e89dd
itzpeckin <justinrmandaro@gmail.com>
2026-08-05 22:58:32 -0500

    5.9.0.5 - Platform

--- platform/core.js
+++ platform/core.js
@@ -17,11 +17,11 @@
   const metadata = Object.freeze({
     name: 'Franchise HQ',
     architecture: 'Frontend Architecture v2',
-    version: '5.9.0.4a',
+    version: '5.9.0.5',
     release: 5,
     epic: 9,
     patch: 1,
-    build: 'madden-companion-import-history-events'
+    build: 'madden-companion-import-framework-certified'
   });
 
   const REQUIRED_SERVICES = Object.freeze([
