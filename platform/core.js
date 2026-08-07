7631d2033d4b77de871e8f218a14fae21497fd36
itzpeckin <justinrmandaro@gmail.com>
2026-08-06 22:37:26 -0500

    5.9.2.1 - Platform

--- platform/core.js
+++ platform/core.js
@@ -17,11 +17,11 @@
   const metadata = Object.freeze({
     name: 'Franchise HQ',
     architecture: 'Frontend Architecture v2',
-    version: '5.9.2.0',
+    version: '5.9.2.1',
     release: 5,
     epic: 9,
     patch: 1,
-    build: 'league-tenant-foundation'
+    build: 'companion-storage-layer'
   });
 
   const REQUIRED_SERVICES = Object.freeze([
