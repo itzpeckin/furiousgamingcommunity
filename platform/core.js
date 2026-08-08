2ea2e22591762f607ddca4ae92858a63fc2d7ca0
itzpeckin <justinrmandaro@gmail.com>
2026-08-08 01:25:12 -0500

    5.9.3.5

--- platform/core.js
+++ platform/core.js
@@ -17,11 +17,11 @@
   const metadata = Object.freeze({
     name: 'Franchise HQ',
     architecture: 'Frontend Architecture v2',
-    version: '5.9.3.4',
+    version: '5.9.3.5',
     release: 5,
     epic: 9,
     patch: 1,
-    build: 'canonical-schedule-game-mapper'
+    build: 'canonical-statistics-engine'
   });
 
   const REQUIRED_SERVICES = Object.freeze([
