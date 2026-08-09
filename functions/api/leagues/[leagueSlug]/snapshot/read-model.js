a582d8983f5251782c5ada181a375bc7062a881f
itzpeckin <justinrmandaro@gmail.com>
2026-08-09 01:58:42 -0500

    Add files via upload

--- functions/api/leagues/[leagueSlug]/snapshot/read-model.js
+++ functions/api/leagues/[leagueSlug]/snapshot/read-model.js
@@ -1,6 +1,6 @@
 import { json, database, normalizeLeagueSlug, validLeagueSlug, resolveLeague } from '../../../../_lib/cloud-platform.js';
 
-const RELEASE = '5.9.4.1b';
+const RELEASE = '5.9.4.2';
 const ALLOWED_DOMAINS = new Set(['teams','players','games','statistics','standings']);
 
 const parse = value => {
