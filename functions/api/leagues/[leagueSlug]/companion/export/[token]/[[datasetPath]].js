3bfe8e3539565dd29dbce4dcaa299fad54620bb3
itzpeckin <justinrmandaro@gmail.com>
2026-08-16 01:21:30 -0500

    Add files via upload

--- functions/api/leagues/[leagueSlug]/companion/export/[token]/[[datasetPath]].js
+++ functions/api/leagues/[leagueSlug]/companion/export/[token]/[[datasetPath]].js
@@ -13,7 +13,7 @@ import {
   summarizePayloadShape
 } from '../../../../../../_lib/cloud-platform.js';
 
-const RELEASE = '5.9.10.6.2d';
+const RELEASE = '5.9.10.6.2e';
 const ALLOWED_METHODS = new Set(['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS']);
 
 function slugOf(context) {
