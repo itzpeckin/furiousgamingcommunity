4783492e21147837fe47ad97a03a0680f535a993
itzpeckin <justinrmandaro@gmail.com>
2026-08-19 22:37:54 -0500

    Add files via upload

--- functions/api/leagues/[leagueSlug]/companion/change-check.js
+++ functions/api/leagues/[leagueSlug]/companion/change-check.js
@@ -1,7 +1,7 @@
 import { json, database, normalizeLeagueSlug, validLeagueSlug, resolveLeague } from '../../../../_lib/cloud-platform.js';
 import { requireCommissioner } from '../../../../_lib/permissions.js';
 
-const RELEASE='5.9.10.6.5.1';
+const RELEASE='5.9.10.6.5.1a';
 
 export async function onRequestGet(context){
   const slug=normalizeLeagueSlug(context);
