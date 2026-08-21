d792d18779b8cd03bef1e18eaceff2a59b4d9b34
itzpeckin <justinrmandaro@gmail.com>
2026-08-21 15:14:25 -0500

    Add files via upload

--- league-engine/one-click-import.js
+++ league-engine/one-click-import.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const HQ = window.FranchiseHQ;
-  const VERSION = '5.9.10.6.5.4e';
+  const VERSION = '5.9.10.6.5.4h-r1';
   const STAGES = [
     ['discover','Discover Latest Companion Captures'],
     ['storage-preflight','Prepare Import Storage'],
