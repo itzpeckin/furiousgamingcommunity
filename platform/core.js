9640933d7138126d8aaabd2482d42ed452865b58
itzpeckin <justinrmandaro@gmail.com>
2026-08-24 13:22:54 -0500

    Add files via upload

--- platform/core.js
+++ platform/core.js
@@ -17,7 +17,7 @@
   const metadata = Object.freeze({
     name: 'Franchise HQ',
     architecture: 'Frontend Architecture v2',
-    version: '6.1.2.4',
+    version: '6.1.2.5',
     release: 6,
     epic: 1,
     patch: 0,
