f35e6ecc8b40efdfa2759a54947483053843a1a6
itzpeckin <justinrmandaro@gmail.com>
2026-09-12 12:32:24 -0500

    Release FranchiseHQ 7.5.5.9

--- functions/leagues/index.js
+++ functions/leagues/index.js
@@ -6,7 +6,7 @@ import {
 } from "../_lib/auth.js";
 import { isOwnerFallbackHost } from "../_lib/origin.js";
 
-const RELEASE = "7.5.5.8";
+const RELEASE = "7.5.5.9";
 
 function esc(value) {
   return String(value ?? "")
