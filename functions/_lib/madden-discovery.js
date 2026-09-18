bf144ca8e277d0ccaf3c2d8e86ee05bea56bff9e
Justin Mandaro <justin.mandaro@lansweeper.com>
2026-09-18 09:22:47 -0500

    Release 7.5.7.6 checkpointed snapshot build

--- functions/_lib/madden-discovery.js
+++ functions/_lib/madden-discovery.js
@@ -2,7 +2,7 @@ import { maddenRoutePeriod, resolveMaddenPeriod, resolveMaddenSchedulePeriods } 
 import { compareSchedulePeriods, currentStatePeriodEvidence, proveCurrentSchedulePeriod } from './schedule-integrity.js';
 
 export const MADDEN_DISCOVERY_ANALYSIS_POLICY = 'roster-carry-forward-v5';
-export const MADDEN_DISCOVERY_RELEASE = '7.5.7.5';
+export const MADDEN_DISCOVERY_RELEASE = '7.5.7.6';
 
 const DATASET_ORDER = Object.freeze([
   'league-info',
--- functions/api/auth/session/claim.js
+++ functions/api/auth/session/claim.js
@@ -6,7 +6,7 @@ import {
   jsonResponse
 } from "../../../_lib/auth.js";
 
-const RELEASE = "7.5.7.5";
+const RELEASE = "7.5.7.6";
 
 export async function onRequestGet(context) {
   return jsonResponse({
